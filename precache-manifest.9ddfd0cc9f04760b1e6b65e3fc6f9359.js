581114ebae50e477c47691fbb62efd2a9f23b9a4
kanokenji <aleksandrtarsevich77@gmail.com>
2020-05-24 10:38:40 -0600

    Updates

---
+++ precache-manifest.9ddfd0cc9f04760b1e6b65e3fc6f9359.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a0eaa826701ec21eb8c7295f719f34e",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "47314862803ff192d5f0",
+    "url": "/todo-list/static/css/main.f7e665cf.chunk.css"
+  },
+  {
+    "revision": "fdf07b6a10668b5c72f4",
+    "url": "/todo-list/static/js/2.cbb04ab1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list/static/js/2.cbb04ab1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47314862803ff192d5f0",
+    "url": "/todo-list/static/js/main.48c00978.chunk.js"
+  },
+  {
+    "revision": "2b909f8b49d22c3e9c72",
+    "url": "/todo-list/static/js/runtime-main.5e1967fb.js"
+  }
+]);
