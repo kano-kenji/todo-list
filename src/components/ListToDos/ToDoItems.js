96f897ad2a3751dda0304f491ac8d42df7e965a5
kanokenji <aleksandrtarsevich77@gmail.com>
2020-05-24 10:50:25 -0600

    Home Work - 4 (ToDo list). Build gh pages branch

--- src/components/ListToDos/ToDoItems.js
+++ src/components/ListToDos/ToDoItems.js
@@ -3,9 +3,7 @@ import '../../App.css';
 
 function ToDoItems(props) {
     return (
-
-            // <li className="completed">
-            <li>
+            <li className="completed">
                 <div className="form-check">
                     <label className="form-check-label">
                         <input className="checkbox" type="checkbox"/>
