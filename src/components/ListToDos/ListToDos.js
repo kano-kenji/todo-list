2b8994f0cd11e2496c65f4a3818a5aef20af0b5a
kanokenji <aleksandrtarsevich77@gmail.com>
2020-05-27 09:48:21 -0600

    Home Work - 4 (ToDo list). Some changes after a lesson

--- src/components/ListToDos/ListToDos.js
+++ src/components/ListToDos/ListToDos.js
@@ -1,16 +1,14 @@
-import React, {Suspense} from 'react';
+import React from 'react';
 import ToDoItem from "./ToDoItem";
 
 function ListToDos(props) {
     return (
         <div className="list-wrapper">
-            <Suspense fallback={<div>Загрузка...</div>}>
-                <ul className="d-flex flex-column-reverse todo-list">
-                    {props.toDos.map((todo, key) =>
-                        <ToDoItem key={key} updateToDo={props.updateToDo} removeToDo={props.removeToDo} toDo={todo}/>
-                    )}
-                </ul>
-            </Suspense>
+            <ul className="d-flex flex-column-reverse todo-list">
+                {props.toDos.map((todo, key) =>
+                    <ToDoItem key={key} updateToDo={props.updateToDo} removeToDo={props.removeToDo} toDo={todo}/>
+                )}
+            </ul>
         </div>
     );
 }
